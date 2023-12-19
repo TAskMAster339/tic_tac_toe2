d1200aae9474654c4009ada80244bad71a1c7b78
Dima <dimagr3214@gmail.com>
2023-12-19 22:44:29 +0300

    Tic_Tac_Toe(v0.2)

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ const blue = 'rgb(0, 191, 255)';
 const marker = 'rgb(0, 40, 255)';
 const block_classic_border = '2px solid gold';
 const block_border = '2px solid ' + marker;
-const box_classic_border = '3px solid red';
-const box_border = '3px solid ' + marker;
+const Box_classic_border = '3px solid red';
+const Box_border = '3px solid ' + marker;
 
 //Массив для проверки условия победы.
 const winArray = [
@@ -44,21 +44,21 @@ function App() {
   let [lastId, setLastId] = useState('');
   let [text, setText] = useState('');
   //Меняет цвет блока, переключая его при ходе.
-  function changeColor(sid, box_id){
+  function changeColor(sid, Box_id){
     //Переменный, используемые в функции.
     const blockID = sid[9]; // id блока внутри бокса на который кликнули
     const BoxID = sid[11]; // id бокса
     const winTable = document.getElementById('winTable'); // окно победы
     const winButton = document.getElementById('restartButton'); // кнопка рестарта
-    const box = document.getElementById(box_id); // Бокс в котором мы находимся
+    const Box = document.getElementById(Box_id); // Бокс в котором мы находимся
     const elem = document.getElementById(sid); // Блок, на который мы кликаем
     const prevElem = document.getElementById(lastId); //Блок, на который мы в прошлый раз кликнули(по умолчанию == NUll)
     const nextBox = document.getElementById(number_trans[blockID]); // Следующий Бокс  здесь sid[9](blockID) -- id следующего бокса(нажатой клетки)
     var typecolor = window.getComputedStyle(elem, null).backgroundColor; //Цвет заднего фона блока
-    var activebox = window.getComputedStyle(box, null).border; // Параметры границ бокса(9 блоков)
+    var activeBox = window.getComputedStyle(Box, null).border; // Параметры границ бокса(9 блоков)
 
     //проверка, что кликаем не на закрашенный блок и на бокс с синим контуром
-    if (typecolor === 'rgba(0, 0, 0, 0)' && activebox === box_border  && !IsWinner){ 
+    if (typecolor === 'rgba(0, 0, 0, 0)' && activeBox === Box_border  && !IsWinner){ 
       if (lastColor === blue){ //определяем цвет, с помощью предыдущего, чтобы не перекрашивать блоки.
         typecolor = red;
         setLastColor(typecolor);
@@ -71,44 +71,42 @@ function App() {
         setLastId(sid);
         winArray[+BoxID - 1][+blockID - 1] = 1; //Отмечаем ход синего в матрице ходов.
     }
-    box.style.border = box_classic_border; // Здесь в box храниться предыдущий элемент, у которого граница возвращается к норме.
-    nextBox.style.border = box_border; // Здесь мы меняем цвет следующего бокса
+    Box.style.border = Box_classic_border; // Здесь в Box храниться предыдущий элемент, у которого граница возвращается к норме.
+    nextBox.style.border = Box_border; // Здесь мы меняем цвет следующего бокса
     elem.style.backgroundColor = typecolor;
     elem.style.border = block_border; // перекрашиваем границу последнего кликнутого блока
     if (prevElem != null){ //возвращаем цвет границы назад
       prevElem.style.border = block_classic_border;
     }
-    console.log(blockID, BoxID, winArray); // Отладка
+    console.log(blockID, BoxID, winArray[+BoxID -1 ]); // Отладка
     }
     if (!IsWinner){  
-      for (let i=0; i < 9; i++){
-        
-          for (let x=-1; x < 2; x+=2){
-            // Перебор всех выигрышных комбинаций как для красного, так и для синего игрока.
-          if ((winArray[i][0] === winArray[i][1] && winArray[i][1] === winArray[i][2] && winArray[i][1] === x) ||
-          (winArray[i][3] === winArray[i][4] && winArray[i][4] === winArray[i][5]  && winArray[i][4] === x) || 
-          (winArray[i][6] === winArray[i][7] && winArray[i][7] === winArray[i][8]  && winArray[i][7] === x) ||
-          (winArray[i][0] === winArray[i][3] && winArray[i][3] === winArray[i][6]  && winArray[i][3] === x) ||
-          (winArray[i][1] === winArray[i][4] && winArray[i][4] === winArray[i][7]  && winArray[i][4] === x) || 
-          (winArray[i][2] === winArray[i][5] && winArray[i][5] === winArray[i][8]  && winArray[i][5] === x) ||
-          (winArray[i][0] === winArray[i][4] && winArray[i][4] === winArray[i][8]  && winArray[i][4] === x) || 
-          (winArray[i][2] === winArray[i][4] && winArray[i][4] === winArray[i][6]  && winArray[i][4] === x)) {
-            if (x === -1){
-              console.log('Red wins');
-              IsWinner = true;
-              winTable.style.backgroundColor = red;
-              winTable.style.display = "block";
-              winButton.style.backgroundColor = blue;
-              setText('Red won');
-            }
-            if (x === 1){
-              console.log('Blue wins');
-              IsWinner = true;
-              winTable.style.backgroundColor = blue;
-              winTable.style.display = 'block';
-              winButton.style.backgroundColor = red;
-              setText("Blue won");
-            }
+        for (let x=-1; x < 2; x+=2){
+          // Перебор всех выигрышных комбинаций как для красного, так и для синего игрока.
+          // +BoxID - 1 индекс бокса, в котором был совершен последний(предположительно победный) ход.
+        if ((winArray[+BoxID - 1][0] === winArray[+BoxID - 1][1] && winArray[+BoxID - 1][1] === winArray[+BoxID - 1][2] && winArray[+BoxID - 1][1] === x) ||
+        (winArray[+BoxID - 1][3] === winArray[+BoxID - 1][4] && winArray[+BoxID - 1][4] === winArray[+BoxID - 1][5]  && winArray[+BoxID - 1][4] === x) || 
+        (winArray[+BoxID - 1][6] === winArray[+BoxID - 1][7] && winArray[+BoxID - 1][7] === winArray[+BoxID - 1][8]  && winArray[+BoxID - 1][7] === x) ||
+        (winArray[+BoxID - 1][0] === winArray[+BoxID - 1][3] && winArray[+BoxID - 1][3] === winArray[+BoxID - 1][6]  && winArray[+BoxID - 1][3] === x) ||
+        (winArray[+BoxID - 1][1] === winArray[+BoxID - 1][4] && winArray[+BoxID - 1][4] === winArray[+BoxID - 1][7]  && winArray[+BoxID - 1][4] === x) || 
+        (winArray[+BoxID - 1][2] === winArray[+BoxID - 1][5] && winArray[+BoxID - 1][5] === winArray[+BoxID - 1][8]  && winArray[+BoxID - 1][5] === x) ||
+        (winArray[+BoxID - 1][0] === winArray[+BoxID - 1][4] && winArray[+BoxID - 1][4] === winArray[+BoxID - 1][8]  && winArray[+BoxID - 1][4] === x) || 
+        (winArray[+BoxID - 1][2] === winArray[+BoxID - 1][4] && winArray[+BoxID - 1][4] === winArray[+BoxID - 1][6]  && winArray[+BoxID - 1][4] === x)) {
+          if (x === -1){
+            console.log('Red wins');
+            IsWinner = true;
+            winTable.style.backgroundColor = red;
+            winTable.style.display = "block";
+            winButton.style.backgroundColor = blue;
+            setText('Red won');
+          }
+          if (x === 1){
+            console.log('Blue wins');
+            IsWinner = true;
+            winTable.style.backgroundColor = blue;
+            winTable.style.display = 'block';
+            winButton.style.backgroundColor = red;
+            setText("Blue won");
           }
         }
       }
